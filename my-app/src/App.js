2c1454e6951642b90c6b5773dfc65f6f205cdf0e
yonatanhb <92885822+yonatanhb@users.noreply.github.com>
2024-05-26 20:16:02 +0300

    add nav bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './login/AuthContext';
-import Login from './login/Login';
+import Login from './login/loginPage';
 import PersonalArea from './pages/PersonalArea';
 import PrivateRoute from './login/PrivateRoute'; // Match the file name exactly
-
 
 function App() {
   return (
