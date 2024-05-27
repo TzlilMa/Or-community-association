d016a8a132c4d1cfcfa86c46191ab1ea77f9a2ea
yonatanhb <92885822+yonatanhb@users.noreply.github.com>
2024-05-27 23:14:41 +0300

    delete "hey" from personal

--- my-app/src/components/PeronalArea.js
+++ my-app/src/components/PeronalArea.js
@@ -4,7 +4,6 @@ import UserForm from './UserForm'; // Adjust the import path as needed
 const HeyComponent = () => {
   return (
     <div>
-      <p>Hey</p>
       <UserForm username="exampleUsername" /> {/* Replace with the actual username */}
     </div>
   );
