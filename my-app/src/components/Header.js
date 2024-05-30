b036e96745f788067ceeed964c0e4e52ea8f5c0e
yonatanhb <92885822+yonatanhb@users.noreply.github.com>
2024-05-30 23:32:38 +0300

    update header

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { auth } from '../fireBase/firebase';
 import { useNavigate } from 'react-router-dom';
-import Logo from './Logo';
+import logo from '../assets/logo.jpeg';
 import InstagramLogo from '../assets/brand-instagram.png';
-import FacebookLogo from '../assets/brand-facebook.png'; 
-import '../styles/Header.css'; 
+import FacebookLogo from '../assets/brand-facebook.png';
+import '../styles/Header.css';
 
 const Header = ({ user, onComponentChange }) => {
   const navigate = useNavigate();
@@ -19,15 +19,23 @@ const Header = ({ user, onComponentChange }) => {
   };
 
   const handleComponentClick = (componentName) => {
-    // Call the parent component's function to change the active component
     onComponentChange(componentName);
+  };
+
+  // Function to handle logo click
+  const handleLogoClick = () => {
+    if (user) {
+      // If user is logged in, navigate to homepage
+      handleComponentClick('ContentLayout');
+    } else {
+      // If user is not logged in, navigate to login page
+      navigate('/login');
+    }
   };
 
   return (
     <header className="header">
       <div className="left-section">
-        {user && <button id="exit" onClick={handleLogout}>התנתק</button>}
-        {user && <p className="greeting">שלום {user.firstName}</p>}
         <div className="social-icons">
           <a href="https://www.facebook.com/share/46HcXMT56FM73K8j/?mibextid=K35XfP" target="_blank" rel="noopener noreferrer">
             <img src={FacebookLogo} alt="Facebook" className="social-logo" />
@@ -36,21 +44,31 @@ const Header = ({ user, onComponentChange }) => {
             <img src={InstagramLogo} alt="Instagram" className="social-logo" />
           </a>
         </div>
+        {user && (
+          <>
+            <p className="greeting">שלום {user.firstName}</p>
+            <button className="logout-btn" onClick={handleLogout}>התנתק</button>
+          </>
+        )}
       </div>
       
-      {user && ( // Conditionally render the buttons if user is logged in
-        <div className="center-section">
-          <button id="chat-btn" onClick={() => handleComponentClick('Chat')}>לוח מודעות</button>
-          <button id="chat-btn" onClick={() => handleComponentClick('Notices')}>צ'אט</button>
-          <button id="stories-btn">סיפורים</button>
-          <button id="events-btn " onClick={() => handleComponentClick('Calendar')}>אירועים</button>
-          <button id="conact-btn">צור קשר</button>
-          <button id="personal-area-btn" onClick={() => handleComponentClick('PersonalArea')}>האזור האישי</button>
-        </div>
-      )}
+      <div className="center-section">
+        {user && (
+          <>
+            <button className="nav-btn" onClick={() => handleComponentClick('Chat')}>לוח מודעות</button>
+            <button className="nav-btn" onClick={() => handleComponentClick('Notices')}>צ'אט</button>
+            <button className="nav-btn" onClick={() => handleComponentClick('Stories')}>סיפורים</button>
+            <button className="nav-btn" onClick={() => handleComponentClick('Calendar')}>אירועים</button>
+            <button className="nav-btn" onClick={() => handleComponentClick('Contact')}>צור קשר</button>
+            <button className="nav-btn" onClick={() => handleComponentClick('PersonalArea')}>האזור האישי</button>
+          </>
+        )}
+      </div>
 
       <div className="right-section">
-        <Logo />
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo-image" onClick={handleLogoClick} />
+        </div>
       </div>
     </header>
   );
