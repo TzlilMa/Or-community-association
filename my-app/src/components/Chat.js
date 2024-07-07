129b8ae050b27673a7ffb88eecf904369545572a
yonatanhb <92885822+yonatanhb@users.noreply.github.com>
2024-07-07 23:48:38 +0300

    chat v2

--- my-app/src/components/Chat.js
+++ my-app/src/components/Chat.js
@@ -11,7 +11,6 @@ const Chat = () => {
   const [newMessage, setNewMessage] = useState("");
   const [loading, setLoading] = useState(true);
   const [sessionIp, setSessionIp] = useState('');
-  const [sessionPort, setSessionPort] = useState('');
   const conversationEndRef = useRef(null);
   const userCache = useRef({});
 
