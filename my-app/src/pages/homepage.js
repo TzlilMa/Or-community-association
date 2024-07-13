7d8d3d4cf583acdda6e21f3d466f15305f3e80b1
yonatanhb <92885822+yonatanhb@users.noreply.github.com>
2024-07-13 23:44:28 +0300

    update homepage

--- my-app/src/pages/homepage.js
+++ my-app/src/pages/homepage.js
@@ -1,18 +1,22 @@
-// src/pages/homepage.js
 import React, { useEffect, useState } from "react";
-import { db, doc, getDoc } from "../fireBase/firebase";
+import { db, doc, getDoc, collection, query, where, getDocs } from "../fireBase/firebase";
 import BulletinBoard from "../components/NotificationComponent/BulletinBoard";
 import ImageSlider from "../components/NotificationComponent/ImageSlider";
 import Management from "../components/Management/Management";
 import InstagramPhotos from "../components/InstagramPhotos";
+import StoryCarousel from "../components/StoryCarousel"; // Ensure this import is correct
 import { useAuth } from "../fireBase/AuthContext";
 import "../styles/Homepage.css";
+import "../styles/BulletinBoard.css";
 import "../styles/ImageSlider.css";
 import "../styles/InstagramPhotos.css";
+import "../styles/StoryCarousel.css";
+import "../styles/storyCard.css";
 
 const Homepage = () => {
   const { currentUser } = useAuth();
   const [showEditButtons, setShowEditButtons] = useState(false);
+  const [stories, setStories] = useState([]);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -22,46 +26,71 @@ const Homepage = () => {
           return;
         }
 
-        console.log("Fetching user data for email:", currentUser.email);
-
         const userDoc = await getDoc(doc(db, "users", currentUser.email));
         if (userDoc.exists()) {
           const userData = userDoc.data();
-          console.log("User data:", userData);
           setShowEditButtons(userData.isAdmin);
         } else {
-          console.error(
-            "User document does not exist for email:",
-            currentUser.email
-          );
+          console.error("User document does not exist for email:", currentUser.email);
         }
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
 
+    const fetchStories = async () => {
+      try {
+        const q = query(collection(db, "users"), where("isStoryPublic", "==", true));
+        const querySnapshot = await getDocs(q);
+        const storyData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          userId: doc.data().userId, // Ensure userId is included
+          name: `${doc.data().firstName} ${doc.data().lastName}`,
+          story: doc.data().personalStory,
+        }));
+        console.log("Fetched stories:", storyData); // Add this line to check fetched stories
+        setStories(storyData);
+      } catch (error) {
+        console.error("Error fetching stories:", error);
+      }
+    };
+
     fetchUserData();
+    fetchStories();
   }, [currentUser]);
 
   return (
     <div className="App">
       <div className="homepage">
-        <div className="main-content">
+        <div className="homepage-main-content">
           <h1>"לעיתים צריכים רק אור קטן בשביל לעשות שינוי גדול"</h1>
-          <div className="content-container">
-            <div className="instagram-photos-container">
-              <InstagramPhotos />
-            </div>
-            <div className="bulletin-board-container">
-              <BulletinBoard showEditButtons={showEditButtons} />
-            </div>
-            <div className="management-container">
-              <Management isAdmin={showEditButtons} />
-            </div>
-            <div className="image-slider-container">
-              <ImageSlider />
+
+          <div className="homepage-section homepage-gradient-background-1">
+            <div className="homepage-content-container">
+              <div className="homepage-component-container">
+                <BulletinBoard showEditButtons={showEditButtons} />
+              </div>
+              <div className="homepage-component-container">
+                <InstagramPhotos />
+              </div>
             </div>
           </div>
+
+          <div className="homepage-section homepage-gradient-background-2">
+            <div className="homepage-content-container">
+              <div className="homepage-component-container">
+                <Management isAdmin={showEditButtons} />
+              </div>
+              <div className="homepage-component-container">
+                <ImageSlider />
+              </div>
+            </div>
+          </div>
+          
+          <div className="homepage-story-carousel-container">
+            <StoryCarousel stories={stories} />
+          </div>
+
         </div>
       </div>
     </div>
